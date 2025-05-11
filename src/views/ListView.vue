1ea310d491def9ce9e03af153bbbeb803c146907
Steven Lehmann <S.Lehmann@Student.HTW-Berlin.de>
2025-05-11 13:53:01 +0200

    Erste eigene Unterkomponente die v-for nutzt

---
+++ src/views/ListView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>This is a List</h1>
+    <li v-for="item in items">
+      {{ item.item_name }} --- {{item.category}}
+    </li>
+  </div>
+  <div>
+
+  </div>
+</template>
+
+
+<script setup lang="ts">
+
+let items: {
+  item_name: string, category: string
+}[] = [
+  {
+    item_name: "Äpfel",
+    category: "Obst und Gemüse"
+  },
+  {
+    item_name: "Zwiebeln",
+    category: "Obst und Gemüse"
+  },
+  {
+    item_name: "Toilettenpapier",
+    category: "Haushaltsmittel"
+  },
+  {
+    item_name: "Orangensaft",
+    category: "Getränke"
+  },
+  {
+    item_name: "Apfelsaft",
+    category: "Getränke"
+  }
+];
+</script>
