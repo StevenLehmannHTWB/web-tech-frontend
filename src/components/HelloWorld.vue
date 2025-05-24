f2e702a87fc498974690766e8bf45f71bfff5545
Steven Lehmann <S.Lehmann@Student.HTW-Berlin.de>
2025-05-24 17:40:15 +0200

    Zugriff auf Backend

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,16 @@
 defineProps<{
   msg: string
 }>()
+
+async function loadThings (owner: string = '') {
+  const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL // 'http://localhost:8080' in dev mode
+  const endpoint = baseUrl + '/things' + '?owner=' + owner
+  const response: AxiosResponse = await axios.get(endpoint);
+  const responseData: Thing[] = response.data;
+  responseData.forEach((thing: Thing) => {
+    items.value.push(thing)
+  })
+}
 </script>
 
 <template>
